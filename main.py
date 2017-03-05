4105ecae59f4fd269317ac7b720245f79d2a8973
lectorvin <smert.s.kosoi@gmail.com>
2017-03-06 01:23:30 +0300

    add titlebar, adding directory etc

--- main.py
+++ main.py
@@ -16,15 +16,23 @@ icons = {'main': 'icons/main.png',
          'volume0': 'icons/volume0.png',
          'volume1': 'icons/volume1.png',
          'volume2': 'icons/volume2.png',
-         'volume3': 'icons/volume3.png'}
+         'volume3': 'icons/volume3.png',
+         'close': 'icons/close.png',
+         'minimize': 'icons/minimize.png',
+         'plus': 'icons/plus.png',
+         'minus': 'icons/minus.png'}
 
 
-class Player(QtGui.QDialog):
+class Player(QtGui.QWidget):
     def __init__(self, parent=None):
-        super(Player, self).__init__(parent)
+        QtGui.QWidget.__init__(self, parent)
         self.plist = None
+        self.m_titlebar = TitleBar("Simple Player", parent=self,
+                                   child=self.plist)
+        self.setWindowFlags(QtCore.Qt.WindowMinimizeButtonHint |
+                            QtCore.Qt.FramelessWindowHint)
 
-        self.resize(250, 140)
+        self.resize(250, 160)
         self.setFixedSize(self.size())
         screen = QtGui.QDesktopWidget().screenGeometry()
         size = self.geometry()
@@ -33,16 +41,19 @@ class Player(QtGui.QDialog):
         self.setWindowTitle("Simple player")
         self.setWindowIcon(QtGui.QIcon(icons['main']))
         self.prev_time = 0
-        button_size = QtCore.QSize(20, 20)
+        self.prev_state = 'visible'
+        button_size = QtCore.QSize(10, 10)
 
         self.play_button = QtGui.QPushButton(QtGui.QIcon(icons['play']), '')
         self.play_button.clicked.connect(self._play)
         self.play_button.setIconSize(button_size)
         self.play_button.setShortcut(QtGui.QKeySequence(QtCore.Qt.Key_Space))
+        self.play_button.setFocusPolicy(QtCore.Qt.NoFocus)
         stop_button = QtGui.QPushButton(QtGui.QIcon(icons['stop']), '')
         stop_button.clicked.connect(self._stop)
         stop_button.setIconSize(button_size)
         stop_button.setShortcut(QtGui.QKeySequence(QtCore.Qt.Key_S))
+        stop_button.setFocusPolicy(QtCore.Qt.NoFocus)
 
         self.state = QtGui.QLabel()
         self.state.setPixmap(self.smallPixmap(icons['stop_in_circle']))
@@ -67,6 +78,7 @@ class Player(QtGui.QDialog):
         self.volume_label = QtGui.QLabel()
         self.volume_label.setPixmap(self.smallPixmap(icons['volume3']))
 
+        content = QtGui.QWidget(self)
         hbox1 = QtGui.QHBoxLayout()
         hbox2 = QtGui.QHBoxLayout()
         hbox5 = QtGui.QHBoxLayout()
@@ -80,13 +92,17 @@ class Player(QtGui.QDialog):
         hbox5.addStretch(1)
         hbox5.addWidget(self.play_button)
         hbox5.addWidget(stop_button)
-
-        vbox = QtGui.QVBoxLayout()
+        vbox = QtGui.QVBoxLayout(content)
         vbox.addLayout(hbox1)
         vbox.addLayout(hbox2)
         vbox.addWidget(self.progress)
         vbox.addLayout(hbox5)
-        self.setLayout(vbox)
+
+        vbox2 = QtGui.QVBoxLayout(self)
+        vbox2.addWidget(self.m_titlebar)
+        vbox2.setContentsMargins(0, 0, 0, 0)
+        vbox2.setSpacing(0)
+        vbox2.addWidget(content)
 
         self.media = Phonon.MediaObject()
         audio = Phonon.AudioOutput(Phonon.MusicCategory, self)
@@ -103,17 +119,32 @@ class Player(QtGui.QDialog):
         self.setStyleSheet(style.main)
         self.pl.setStyleSheet(style.playlist_button)
 
+    def playlist(self, parent=None):
+        self.plist.showFullScreen()
+
     def _play(self):
         if self.media.state() == Phonon.PlayingState:
             if self.volume.value() == 0:
                 self.volume_label.setPixmap(self.smallPixmap(icons['volume0']))
             self.media.pause()
             self.play_button.setIcon(QtGui.QIcon(icons['play']))
-        else:
+        elif self.media.state() in [Phonon.StoppedState, Phonon.PausedState]:
             if self.volume.value() == 0:
                 self.volume_label.setPixmap(self.smallPixmap(icons['volume-']))
-            self.media.play()
-            self.play_button.setIcon(QtGui.QIcon(icons['pause']))
+            if self.media.currentSource().type() == Phonon.MediaSource.Empty:
+                if len(self.plist.stringList.stringList()) > 0:
+                    if len(self.plist.song_list.selectedIndexes()) > 0:
+                        self.set_song(self.plist.song_list.selectedIndexes()[0])
+                    else:
+                        index = self.plist.stringList.createIndex(0, 0)
+                        self.plist.song_list.setCurrentIndex(index)
+                        self.set_song(index)
+            else:
+                self.media.play()
+                self.play_button.setIcon(QtGui.QIcon(icons['pause']))
+        else:
+            self.message('Error', 'Unexpected error {}!'.format(
+                self.media.state()))
 
     def _stop(self):
         if self.volume.value() == 0:
@@ -121,6 +152,7 @@ class Player(QtGui.QDialog):
         self.play_button.setIcon(QtGui.QIcon(icons['play']))
         self.media.stop()
         self.title.setText("Simple Player")
+        self.media.clear()
 
     def message(self, name, message):
         reply = QtGui.QMessageBox.question(self, name, message,
@@ -178,11 +210,12 @@ class Player(QtGui.QDialog):
         small_size = QtCore.QSize(20, 20)
         return QtGui.QPixmap(path).scaled(small_size, QtCore.Qt.KeepAspectRatio)
 
-    def set_song(self, name):
+    def set_song(self, index):
+        self.plist.song_list.setCurrentIndex(index)
         self._stop()
-        self.title.setText(name.data())
+        self.title.setText(index.data())
         self.media.setCurrentSource(Phonon.MediaSource(
-            self.plist.files[name.row()]))
+            self.plist.files[index.row()]))
         self._play()
 
     def keyPressEvent(self, event):
@@ -193,38 +226,82 @@ class Player(QtGui.QDialog):
         self.plist.close()
         event.accept()
 
-    def playlist(self, parent=None):
-        self.plist.show()
+    def paintEvent(self, event):
+        opt = QtGui.QStyleOption()
+        opt.init(self)
+        painter = QtGui.QPainter(self)
+        self.style().drawPrimitive(QtGui.QStyle.PE_Widget, opt, painter, self)
+
+    def changeEvent(self, event):
+        if event.type() == QtCore.QEvent.WindowStateChange:
+            if self.isVisible() and self.prev_state != 'visible':
+                self.prev_state = 'visible'
+                self.show()
+                self.showMaximized()
+                self.showFullScreen()
+                if self.plist.prev_state == 'show':
+                    self.plist.show()
+                    self.plist.showMaximized()
+                    self.plist.showFullScreen()
+                else:
+                    self.plist.close()
+            if self.isMinimized() and self.prev_state != 'minimized':
+                self.prev_state = 'minimized'
 
 
-class Playlist(QtGui.QDialog):
+class Playlist(QtGui.QWidget):
     def __init__(self, parent=None):
         super(Playlist, self).__init__()
+        self.setWindowTitle("Playlist")
+        self.setWindowFlags(QtCore.Qt.WindowMinimizeButtonHint |
+                            QtCore.Qt.FramelessWindowHint)
         self.player = parent
+        self.player.m_titlebar.child = self
         self.resize(250, 300)
         self.setFixedSize(self.size())
-        self.screen = QtGui.QDesktopWidget().screenGeometry()
         self._files = []
         self._titles = []
+        self.titlebar = TitleBar('Playlist', minimize=False, parent=self)
+        self.titlebar.close_.setShortcut(QtGui.QKeySequence(QtCore.Qt.Key_P))
+        self.prev_state = 'hidden'
 
-        choose = QtGui.QPushButton("Choose file")
-        choose.clicked.connect(self.choose_file)
-        choose.setShortcut(QtGui.QKeySequence(QtCore.Qt.Key_C))
-        choose.setStyleSheet(style.choose)
+        content = QtGui.QWidget(self)
+        plus = QtGui.QPushButton(QtGui.QIcon(icons['plus']), '')
+        plus.setFocusPolicy(QtCore.Qt.NoFocus)
+        menu1 = QtGui.QMenu()
+        menu1.addAction('add song', self.choose_file)
+        menu1.addAction('add directory', self.choose_dir)
+        plus.setMenu(menu1)
+        minus = QtGui.QPushButton(QtGui.QIcon(icons['minus']), '')
+        minus.setFocusPolicy(QtCore.Qt.NoFocus)
+        menu2 = QtGui.QMenu()
+        menu2.addAction('remove selected', self.remove_selected)
+        minus.setMenu(menu2)
         self.song_list = QtGui.QListView()
         self.stringList = QtGui.QStringListModel()
         self.song_list.setModel(self.stringList)
-        self.connect(self.song_list, QtCore.SIGNAL("clicked(QModelIndex)"),
+        self.connect(self.song_list,
+                     QtCore.SIGNAL("doubleClicked(QModelIndex)"),
                      self.player.set_song)
         self.label = QtGui.QLabel()
-        vbox = QtGui.QVBoxLayout()
+        hbox = QtGui.QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(plus)
+        hbox.addWidget(minus)
+        vbox = QtGui.QVBoxLayout(content)
         vbox.addWidget(self.song_list)
-        vbox.addWidget(choose)
-        self.setLayout(vbox)
+        vbox.addLayout(hbox)
+        vbox2 = QtGui.QVBoxLayout(self)
+        vbox2.addWidget(self.titlebar)
+        vbox2.setContentsMargins(0, 0, 0, 0)
+        vbox2.setSpacing(0)
+        vbox2.addWidget(content)
 
-    def showEvent(self, event):
-        self.move((self.screen.width()-500)/2,
-                  (self.screen.height()-85)/2)
+        self.song_list.setStyleSheet(style.list_view)
+        self.setStyleSheet(style.playlist)
+        plus.setStyleSheet(style.small_button)
+        minus.setStyleSheet(style.small_button)
+        # self.set_song(self.plist.song_list.selectedIndexes()[0])
 
     @property
     def files(self):
@@ -239,12 +316,90 @@ class Playlist(QtGui.QDialog):
             self._titles.append(getTitle(path))
             self.stringList.setStringList(self._titles)
 
+    def choose_dir(self):
+        path = QtGui.QFileDialog.getExistingDirectory(self, 'Open directory',
+                                                      '.')
+        if os.path.isdir(path):
+            for x in os.listdir(path):
+                if x.endswith('.mp3'):
+                    self._files.append(os.path.join(path, x))
+                    self._titles.append(getTitle(os.path.join(path, x)))
+            self.stringList.setStringList(self._titles)
 
-def strip(s):
-    s = ''.join([chr(x) for x in s])
-    while len(s) > 0 and s[-1] in [' ', '\0']:
-        s = s[:-1]
-    return s
+    def remove_selected(self):
+        indexes = [x.row() for x in self.song_list.selectedIndexes()]
+        for x in reversed(sorted(indexes)):
+            self._files.pop(x)
+            self._titles.pop(x)
+        self.stringList.setStringList(self._titles)
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Escape:
+            self.close()
+
+    def showEvent(self, event):
+        self.prev_state = 'show'
+        if not self.player.isMinimized():
+            screen = QtGui.QDesktopWidget().screenGeometry()
+            event.accept()
+            self.move((screen.width()-500)/2,
+                      (screen.height()-160)/2)
+        else:
+            self.close()
+            if self.player.prev_state != 'minimized':
+                self.player.show()
+                self.player.showFullScreen()
+
+    def closeEvent(self, event):
+        self.prev_state = 'hidden'
+        event.accept()
+
+
+class TitleBar(QtGui.QDialog):
+    def __init__(self, name, minimize=True, parent=None, child=None):
+        QtGui.QWidget.__init__(self, parent)
+        self.parent = parent
+        self.child = child
+        self.setWindowTitle(name)
+        self.setWindowFlags(QtCore.Qt.FramelessWindowHint)
+        self.setAutoFillBackground(True)
+        self.setBackgroundRole(QtGui.QPalette.Highlight)
+        self.setSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Fixed)
+        self.setStyleSheet(style.titlebar)
+
+        self.close_ = QtGui.QToolButton()
+        self.close_.setIcon(QtGui.QIcon(icons['close']))
+        self.close_.clicked.connect(self.close)
+        self.close_.setFocusPolicy(QtCore.Qt.NoFocus)
+        name = QtGui.QLabel(name)
+        self.maxNormal = False
+
+        hbox = QtGui.QHBoxLayout()
+        hbox.addWidget(name)
+        hbox.addStretch(1)
+        if minimize:
+            self.minimize = QtGui.QToolButton()
+            self.minimize.setIcon(QtGui.QIcon(icons['minimize']))
+            self.minimize.clicked.connect(self.showSmall)
+            self.minimize.setFocusPolicy(QtCore.Qt.NoFocus)
+            hbox.addWidget(self.minimize)
+        hbox.addWidget(self.close_)
+        self.setLayout(hbox)
+
+    def showSmall(self):
+        self.parent.showMinimized()
+        self.child.showMinimized()
+
+    def close(self):
+        self.parent.close()
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Escape:
+            self.close()
+
+
+def strip(s):    # from byte array to string without '\0'
+    return (''.join([chr(x) for x in s if x > 0])).strip()
 
 
 def getTitle(path):
@@ -255,6 +410,7 @@ def getTitle(path):
         return artist + ' - ' + title
     else:
         return os.path.splitext(os.path.basename(path))[0]
+
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
--- style.py
+++ style.py
@@ -1,87 +1,123 @@
-button = " \
-    QPushButton { \
-        border: 1px solid #1f1f91; \
-        border-radius: 15px; \
-        background: qlineargradient(x1:0, y1:0, x2:0, y2:1, \
-                            stop:0 #a8bdff, stop:1 #789daf); \
-        min-width: 30px; \
-        min-height: 30px; \
-        max-width: 30px; \
-        max-height: 30px; \
-    }"
+button = """
+    QPushButton {
+        border: 1px solid #1f1f91;
+        border-radius: 10px;
+        background: qlineargradient(x1:0, y1:0, x2:0, y2:1,
+                            stop:0 #a8bdff, stop:1 #789daf);
+        min-width: 20px;
+        min-height: 20px;
+        max-width: 20px;
+        max-height: 20px;
+    }
+"""
 
-progress = " \
-    QSlider::groove:horizontal { \
-        border: 1px solid #191919; \
-        height: 8px; \
-        background: qlineargradient(x1:0, y1:0, x2:0, y2:1, \
-                            stop:0 #a6a6a6, stop:1 #c1c1c1); \
-        margin: 2px 0; \
-    } \
-        \
-    QSlider::handle:horizontal { \
-        background: qlineargradient(x1:0, y1:0, x2:1, y2:1, \
-                            stop:0 #b4b4b4, stop:1 #8f8f8f); \
-        border: 1px solid #5c5c5c; \
-        width: 18px; \
-        margin: -2px 0; \
-        border-radius: 3px; \
-    }\
-    QSlider::sub-page:horizontal { \
-        border: 1px solid #191919; \
-        height: 8px; \
-        background: #737373; \
-        margin: 2px 0; \
-    }"
+progress = """
+    QSlider::groove:horizontal {
+        border: 1px solid #191919;
+        height: 8px;
+        background: qlineargradient(x1:0, y1:0, x2:0, y2:1,
+                            stop:0 #a6a6a6, stop:1 #c1c1c1);
+        margin: 2px 0;
+    }
 
-progress_without_handle = " \
-    QSlider::groove:horizontal { \
-        border: 1px solid #191919; \
-        height: 8px; \
-        background: qlineargradient(x1:0, y1:0, x2:0, y2:1, \
-                            stop:0 #a6a6a6, stop:1 #c1c1c1); \
-        margin: 2px 0; \
-    }"
+    QSlider::handle:horizontal {
+        background: qlineargradient(x1:0, y1:0, x2:1, y2:1,
+                            stop:0 #b4b4b4, stop:1 #8f8f8f);
+        border: 1px solid #5c5c5c;
+        width: 15px;
+        margin: -2px 0;
+        border-radius: 3px;
+    }
 
-volume = " \
-    QSlider::groove:horizontal { \
-        border: 1px solid #001a66; \
-        height: 4px; \
-        background: qlineargradient(x1:0, y1:0, x2:0, y2:1, \
-                            stop:0 #a88888, stop:1 #a8bdff); \
-        margin: 2px 0; \
-    } \
-    \
-    QSlider::handle:horizontal { \
-        background: qlineargradient(x1:0, y1:0, x2:1, y2:1, \
-                            stop:0 #3b3c3d, stop:1 #3f3f3f); \
-        width: 12px; \
-        margin: -5px 0; \
-        border-radius: 5px; \
-    }"
+    QSlider::sub-page:horizontal {
+        border: 1px solid #191919;
+        height: 8px;
+        background: #737373;
+        margin: 2px 0;
+    }
+"""
 
-choose = " \
-    QPushButton { \
-        border: 1px solid #191919; \
-        background: #333333; \
-        border-radius: 20px; \
-        color: #dce1ef; \
-    }"
+volume = """
+    QSlider::groove:horizontal {
+        border: 1px solid #001a66;
+        height: 4px;
+        background: qlineargradient(x1:0, y1:0, x2:0, y2:1,
+                            stop:0 #a88888, stop:1 #a8bdff);
+        margin: 2px 0;
+    }
 
-playlist_button = " \
-    QPushButton { \
-        border: 1px solid #191919; \
-        background: #444444; \
-        border-radius: 5px; \
-        color: #dce1ef; \
-    }"
+    QSlider::handle:horizontal {
+        background: qlineargradient(x1:0, y1:0, x2:1, y2:1,
+                            stop:0 #3b3c3d, stop:1 #3f3f3f);
+        width: 12px;
+        margin: -5px 0;
+        border-radius: 5px;
+    }
+"""
 
-main = " \
-    background-color: #b7b7b7; \
-    color: #111111; \
-"
+choose = """
+    QPushButton {
+        border: 1px solid #191919;
+        background: #333333;
+        border-radius: 20px;
+        color: #dce1ef;
+    }
+"""
 
-playlist = " \
-    background-color: #c7c7c7; \
-    color: #111111; \
-"
+playlist_button = """
+    QPushButton {
+        border: 1px solid #191919;
+        background: #444444;
+        border-radius: 5px;
+        color: #dce1ef;
+    }
+"""
+
+main = """
+    QWidget {
+        background-color: #b7b7b7;
+        color: #111111;
+    }
+    QWidget::title {
+        background-color: white;
+    }
+"""
+
+playlist = """
+    QWidget {
+        background-color: #d7d7d7;
+        color: #111111;
+    }
+"""
+
+list_view = """
+    QListView {
+        background-color: white;
+        color: black;
+}
+"""
+
+titlebar = """
+    QWidget {
+        background-color: black;
+        color: white;
+        font: 14px;
+        height: 10px;
+    }
+
+    QToolButton {
+        height: 5px;
+        background-color: black;
+    }
+"""
+
+small_button = """
+    QPushButton {
+        border: 1px solid #1f1f91;
+        background: black;
+        min-width: 20px;
+        min-height: 20px;
+        max-width: 20px;
+        max-height: 20px;
+    }
+"""
